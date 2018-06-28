00cff44292a257d06ca79588be4ef99c1a07502d
AnmoLSin <anisaggu8@gmail.com>
2018-06-28 12:57:21 +0530

    login android

---
+++ app/src/main/java/com/example/hp/intent/listactivity.java
@@ -0,0 +1,39 @@
+package com.example.hp.intent;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.hp.intent.R;
+
+public class listactivity extends AppCompatActivity {
+    private TextView textLogin;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_listactivity);
+        Intent i=getIntent();
+        String userName=i.getStringExtra("username");
+
+
+        Button button11 = findViewById(R.id.butto);
+        button11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("ButtonState","Button Clicked");
+                Intent intent = new Intent(listactivity.this,Main2Activity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
